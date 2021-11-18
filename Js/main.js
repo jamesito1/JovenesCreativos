4a8a12852d91b867d07b6e6307e648657ea14756
jamesito1 <jamespesca2004@gmail.com>
2021-11-17 21:46:16 -0500

    se elimino una linea de script en todos los archivos html, se termino el modo oscuro, se aplico en todos los elementos html

--- Js/main.js
+++ Js/main.js
@@ -6,8 +6,13 @@ let cuerpo= document.querySelector('.cuerpo');
 const barra_navegacion =document.querySelector('.barra_navegacion')
 const tabla_hobbies= document.querySelector('.hobbies_tabla')
 const pie=document.querySelector('.pie')
+let habilidades_iconos=document.querySelectorAll('.habilidades_iconos')
+let conocimientos_iconos =document.querySelectorAll('.conocimientos_iconos')
+let contenido_p = document.querySelector('.contenido_p')
+let contenido_p2 = document.querySelector('.contenido_p2')
+let habilidades_lista = document.querySelectorAll('.habilidades_lista')
 
-console.log(btn_noche2)
+console.log(conocimientos_iconos)
 
 btn_noche.addEventListener('click', ()=>{
   if(btn_noche2==1){
@@ -16,12 +21,37 @@ btn_noche.addEventListener('click', ()=>{
   }
 
   if(btn_noche2==2){
-    cuerpo.classList.toggle('cuerpo_dark')
-  }
+    btn_noche.classList.toggle('boton_modo_dia')
+    sol.classList.toggle('modo_dia_activado');
+    luna.classList.toggle('modo_noche_desactivado');
+    barra_navegacion.classList.toggle('barra_navegacion_dark');
+    console.log(barra_navegacion);
+    document.body.classList.toggle('body_dark');
+    pie.classList.toggle('pie_dark');
+    cuerpo.classList.toggle('cuerpo_dark');
+    habilidades_lista[0].classList.toggle('habilidades_lista_dark')
+    habilidades_lista[1].classList.toggle('habilidades_lista_dark')
 
-  if(btn_noche2==3){
+    for(let i=0;i<=7;i++){
+      conocimientos_iconos[i].classList.toggle('conocimientos_iconos_dark');
+      console.log(i)
+      }
+
+    for(let i=0;i<=7;i++){
+    habilidades_iconos[i].classList.toggle('habilidades_iconos_dark');
+    console.log(i)
+    }
     
   }
+if(btn_noche2==3){
+  cuerpo.classList.toggle('cuerpo_dark');
+}
+  
+if(btn_noche2==4){
+  contenido_p.classList.toggle('contenido_p_dark')
+  contenido_p2.classList.toggle('contenido_p2_dark')
+
+}
 
   btn_noche.classList.toggle('boton_modo_dia')
   sol.classList.toggle('modo_dia_activado');
@@ -31,6 +61,6 @@ btn_noche.addEventListener('click', ()=>{
   barra_navegacion.classList.toggle('barra_navegacion_dark')
   
   pie.classList.toggle('pie_dark')
-  console.log(barra_navegacion)
+  console.log(habilidades_iconos)
 
 })
