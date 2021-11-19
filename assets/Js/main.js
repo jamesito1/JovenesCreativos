9cfa4bfd94b8c4e9e4afcba1e5fdb3626d090d25
jamesito1 <jamespesca2004@gmail.com>
2021-11-18 22:08:00 -0500

    Se agrego una nueva funcionalidad popup en la que se muestra una curiosidad aleatoria, la funcionalidad se aplico en habilidades.html

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -64,3 +64,42 @@ if(btn_noche2==4){
   console.log(habilidades_iconos)
 
 })
+
+
+
+const abrir_curiosidad = document.getElementById('abrir_curiosidad');
+const contenedor_curiodidades = document.getElementById('contenedor_curiodidades');
+const cerrar_curiosidad = document.getElementById('cerrar_curiosidad');
+const curiosidad = document.getElementById('curiosidad');
+
+abrir_curiosidad.addEventListener('click', () => {
+  contenedor_curiodidades.classList.add('activar_curiosidad');  
+  let curiosidad_ale = Math.round(Math.random()*10);
+  if(curiosidad_ale==1){
+    curiosidad.innerHTML ="Tengo un gato llamado Happi, es negro y muy bonito"
+  }
+  if(curiosidad_ale==2){
+    curiosidad.innerHTML ="Mi animal favorito son las tortugas, y por mucho tiempo tuve una pero se escapo"
+  }if(curiosidad_ale==3){
+    curiosidad.innerHTML ="Mis colores favoritos son el azul,naraja y verde"
+  }if(curiosidad_ale==4){
+    curiosidad.innerHTML ="Naci en un pueblo que esta a 5 horas de medellin, cuando cumpli un año nos mudamos a medellin"
+  }if(curiosidad_ale==5){
+    curiosidad.innerHTML ="Mi comida favorita es el arroz, con cualquier cosa sabe bien"
+  }if(curiosidad_ale==6){
+    curiosidad.innerHTML ="Mi Superheroe favorito es Deadpool"
+  }if(curiosidad_ale==7){
+    curiosidad.innerHTML ="Tengo un hermano sietes años menor que yo"
+  }if(curiosidad_ale==8){
+    curiosidad.innerHTML ="Me encata volar cometas"
+  }if(curiosidad_ale==9){
+    curiosidad.innerHTML ="Mi pelicula favoritas son: 1917 y mis series favoritas son: Better Call Saul y Breaking Bad"}
+  if(curiosidad_ale==10){
+    curiosidad.innerHTML ="Mis libros favoritos son: Satanas(Mario Mendoza) y El misterio de Salem's Lot "
+  }
+  
+});
+
+cerrar_curiosidad.addEventListener('click', () => {
+  contenedor_curiodidades.classList.remove('activar_curiosidad');
+});
